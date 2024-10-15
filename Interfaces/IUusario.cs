fad550b120a37ef5e8c84234fed9d8da958071f3
Joaquin Ordonez <joacoignacioordonez@gmail.com>
2024-10-14 23:16:50 -0300

    second commit

--- Interfaces/IUusario.cs
+++
@@ -1,9 +0,0 @@
-    public interface IUsuarioService
-    {
-        public IEnumerable<Usuario> GetAll();
-        public Usuario? GetById(int id);
-        public Usuario Create(UsuarioDTO u);
-        public void Delete(int id);
-        public Usuario? Update(int id, Usuario a);
-
-    }
